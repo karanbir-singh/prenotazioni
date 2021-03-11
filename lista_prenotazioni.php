527b6dba8c779cd4592dc146fcf8480cbef1748b
karanbir-singh <karans1723@gmail.com>
2021-03-11 18:16:34 +0100

    Add table format of reservations

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -22,4 +22,18 @@ $sql = "SELECT * FROM prenotazione";
 
 $stmt = $pdo->query($sql);
 
-var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+//var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+$table = '<table style="width: 100%"><tr><th>ID</th><th>Codice fiscale</th><th>Data</th></tr>';
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $id = $row['id'];
+    $codFiscale = $row['codice_fiscale'];
+    $data = $row['giorno'];
+
+    $table = $table . "<tr style='text-align: center'>
+                            <td style='border: 1px solid black'>$id</td>
+                            <td style='border: 1px solid black'>$codFiscale</td>
+                            <td style='border: 1px solid black'>$data</td>
+                        </tr>";
+}
+
+echo $table . '</table>';
