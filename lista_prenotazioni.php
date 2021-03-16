578bef89a2e4325684f54d73b17f84927267b797
karanbir-singh <karans1723@gmail.com>
2021-03-16 12:36:29 +0100

    Add template which diplays the table

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -1,25 +1,43 @@
 <?php
+require './vendor/autoload.php';
 include_once("config.php");
+
+use League\Plates\Engine;
+
+// Viene creato l'oggetto per la gestione dei template
+// view e' la cartella che contiene il template file
+// tpl e' l'estensione del template file
+$templates = new Engine('./view', 'tpl');
 
 // Query di inserimento preparata
 $sql = "SELECT * FROM prenotazione";
 
+// Invio la query al server MySQL
 $stmt = $pdo->query($sql);
 
-//var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
-$table = '<table style="width: 100%"><tr><th>ID</th><th>Codice fiscale</th><th>Data</th><th>Codice prenotazione</th></tr>';
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $id = $row['id'];
-    $codFiscale = $row['codice_fiscale'];
-    $data = $row['giorno'];
-    $uniqid = $row['codice_prenotazione'];
+// Estraggo le righe di risposta che finiranno come vettori in $result
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $table = $table . "<tr style='text-align: center'>
-                            <td style='border: 1px solid black'>$id</td>
-                            <td style='border: 1px solid black'>$codFiscale</td>
-                            <td style='border: 1px solid black'>$data</td>
-                            <td style='border: 1px solid black'>$uniqid</td>
-                        </tr>";
-}
+// Metodo meccanico e non molto ragionevole (potrebbe capitare di perdere dei pezzi)
+/*
+    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+    $table = '<table style="width: 100%"><tr><th>ID</th><th>Codice fiscale</th><th>Data</th><th>Codice prenotazione</th></tr>';
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $id = $row['id'];
+        $codFiscale = $row['codice_fiscale'];
+        $data = $row['giorno'];
+        $uniqid = $row['codice_prenotazione'];
 
-echo $table . '</table>';
+        $table = $table . "<tr style='text-align: center'>
+                                <td style='border: 1px solid black'>$id</td>
+                                <td style='border: 1px solid black'>$codFiscale</td>
+                                <td style='border: 1px solid black'>$data</td>
+                                <td style='border: 1px solid black'>$uniqid</td>
+                            </tr>";
+    }
+
+    echo $table . '</table>';
+*/
+
+//Rendo un template che mi visualizza la tabella
+echo $templates->render('lista_prenotazioni', ['result' => $result]);
---
+++ view/lista_prenotazioni.tpl
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+</head>
+<body>
+<h1>Portale prenotazioni</h1>
+<h2>Lista delle prenotazioni</h2>
+<table style="width: 100%"><tr><th>ID</th><th>Codice fiscale</th><th>Data</th><th>Codice prenotazione</th></tr>
+    <?php foreach($result as $row): ?>
+    <tr>
+        <td><?php echo $row['id'] ?></td>
+        <td><?php echo $row['codice_fiscale'] ?></td>
+        <td><?php echo $row['giorno'] ?></td>
+        <td><?php echo $row['codice_prenotazione'] ?></td>
+    </tr>
+    <?php endforeach ?>
+</table>
+</body>
+</html>
