9d1773f0014d0ab4e93caaeba735b33c77e2eba6
karanbir-singh <karans1723@gmail.com>
2021-03-27 08:49:45 +0100

    Organize files

--- prenota/prenota.php
+++ prenota/prenota.php
@@ -1,6 +1,6 @@
 <?php
-include_once "config.php";
-include_once "phpqrcode/qrlib.php";
+include_once "../config.php";
+include_once "../phpqrcode/qrlib.php"; //http://phpqrcode.sourceforge.net/
 
 // Variabili valorizzate tramite POST
 $codFiscale = $_POST['codFiscale'];
--- prenotazioni_oggi.php
+++ prenotazioni_oggi.php
@@ -1,6 +1,6 @@
 <?php
-require './vendor/autoload.php';
-include_once("config.php");
+require '../vendor/autoload.php';
+include_once "config.php";
 
 use League\Plates\Engine;
 
